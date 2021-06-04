0415ce744c834eb6b7821e9a840801cb13237ed8
mmilano <mmilano+git@gmail.com>
2021-06-04 12:06:52 -0700

    update dom class methods

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,14 @@
 /** DOM Utilities */
 function hasClass(el, className) {
-  return !!el.className.match(new RegExp("(\\s|^)" + className + "(\\s|$)"));
+    return el.classList.contains(className);
 }
 
 function addClass(el, className) {
-  if (!hasClass(el, className)) el.className += " " + className;
+    el.classList.add(className);
 }
 
 function removeClass(el, className) {
-  if (el.classList) {
     el.classList.remove(className);
-  }
 }
 
 export { addClass, removeClass };
